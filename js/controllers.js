8b27f394e42e1b35702392d48ae0fe62f73c76d6
Sean Mikalson <sean.mikalson@gmail.com>
2015-06-15 17:40:55 -0600

    Enlarge photos when clicked and show upload status

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,7 @@
 var fotoApp = angular.module('fotoControllers', []);
 
 fotoApp.controller('FotoHomeCtrl', ['$scope', '$http', function ($scope, $http) {
-    $http.get(config.url).success(function(data, status, headers,config) {
-        console.log('got pictures');
+    $http.get(config.url).success(function(data, status,headers,config) {
         $scope.pictures = data;
     });
 
@@ -15,27 +14,25 @@ fotoApp.directive('fotoFileSelect', ['$http', function($http) {
     return {
         link: function($scope, el, attrs) {
             el.bind('change', function(e) {
+                var button = $('.btn-file').button('sharing');
                 console.log('file selected');
                 $scope.file = e.target.files[0];
                 var fileReader = new FileReader();
 
                 fileReader.onload = function(evt) {
+                    button.button('reset');
                     var image = new Image();
-                    console.log(evt.target.result);
                     image.src = evt.target.result;
-                    console.log(image.width);
-                    $scope.pictures.push[{data:image.src}];
+
+                    $scope.$apply(function() {
+                        $scope.pictures.push({data:image.src});
+                    });
+
                     var cmpImage = compress(image);
-                    $scope.$apply();
                     $http.post(config.url,{data:cmpImage.src});
                 };
 
                 fileReader.readAsDataURL($scope.file);
-
-                $http.get(config.url).success(function(data, status, headers,config) {
-                    console.log('got pictures');
-                    $scope.pictures = data;
-                });
             });
         }
     }
--- js/picUtil.js
+++ js/picUtil.js
@@ -3,10 +3,11 @@ function compress(image) {
     var canvas = document.createElement('canvas');
     canvas.height = image.height;
     canvas.width = image.width;
+
     var ctx = canvas.getContext('2d');
     ctx.drawImage(image,0,0);
     var newImageData = canvas.toDataURL('image/jpeg', 0.3);
-    console.log(newImageData);
+
     var result_image_obj = new Image();
     result_image_obj.src = newImageData;
     return result_image_obj;
